dc783311c8ae214112e9386e5e90cbfada4be5e6
Hartator <hartator@gmail.com>
2012-08-11 23:28:02 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textopia::Application.config.secret_token = '4796c7bcc1f30805dc0ecd222edf340610bf080598b2d6a9d3cfb3430744f3fb543c29ef6f1e481e582cbde026dc7670b43761a79ad27839e6869ee73a0c68f5'
